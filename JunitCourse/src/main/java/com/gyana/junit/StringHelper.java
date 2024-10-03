ccbc23e4f2382324390026bce42b641bd7349cfe
Jnanranjan Behera <105503589+gyanaranjan12345@users.noreply.github.com>
2024-10-04 04:19:55 +0530

    Add files via upload

---
+++ JunitCourse/src/main/java/com/gyana/junit/StringHelper.java
@@ -0,0 +1,31 @@
+package com.gyana.junit;
+
+public class StringHelper {
+	
+     //AACD=>CD  ACD=>CD  CDEF=>CDEF  CDAA=>CDAA
+	public String truncateAInFirst2Positions(String str) {
+		if (str.length() <= 2)
+			return str.replaceAll("A", "");
+
+		String first2Chars = str.substring(0, 2);
+		String stringMinusFirst2Chars = str.substring(2);
+
+		return first2Chars.replaceAll("A", "") 
+				+ stringMinusFirst2Chars;
+	}
+     //ABCD=> false,  ABAB=>true,  AB=>true,   A=>false
+	public boolean areFirstAndLastTwoCharactersTheSame(String str) {
+
+		if (str.length() <= 1)
+			return false;
+		if (str.length() == 2)
+			return true;
+
+		String first2Chars = str.substring(0, 2);
+
+		String last2Chars = str.substring(str.length() - 2);
+
+		return first2Chars.equals(last2Chars);
+	}
+
+}
---
+++ JunitCourse/src/test/java/com/gyana/junit/helper/StringHelperTest.java
@@ -0,0 +1,35 @@
+package com.gyana.junit.helper;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.gyana.junit.StringHelper;
+
+public class StringHelperTest {
+	//AACD=>CD  ACD=>CD  CDEF=>CDEF  CDAA=>CDAA
+	StringHelper helper=new StringHelper();
+
+	@Test
+	public void testTruncateAInFirst2Positions() {
+		assertEquals("CD",helper.truncateAInFirst2Positions("AACD"));
+		//expected,actual
+	}
+	@Test
+	public void testTruncateAInFirst2Positions2() {
+		assertEquals("CD",helper.truncateAInFirst2Positions("ACD"));
+	}
+	
+	//ABCD=> false,  ABAB=>true,  AB=>true,   A=>false
+	@Test
+	public void testAreFirstAndLastTwoCharactersTheSame() {
+		assertEquals(true,helper.areFirstAndLastTwoCharactersTheSame("ABAB"));
+	}
+	
+	@Test
+	public void testAreFirstAndLastTwoCharactersTheSame2() {
+		assertFalse(helper.areFirstAndLastTwoCharactersTheSame("ABCB"));
+		assertTrue(helper.areFirstAndLastTwoCharactersTheSame("TVTV"));
+	}
+    
+}
